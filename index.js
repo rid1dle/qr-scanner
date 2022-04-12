e31fe424c923fa490fd67704e750e66dd5c063e0
Himanshu Lath <lathhimanshu@gmail.com>
2022-04-12 23:05:05 +0530

    Initial

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const path = require("path");
-const qrcode = require("qrcode-generator");
+const QRCode = require("qrcode");
 // const ejs = require("ejs");
 const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 const port = 3000;
 
-// app.use(function (req, res, next) {
-//   // req.active = req.path.split("/")[1];
-//   const active = req.path.split("/")[1];
-//   if (req.length === 0) req.active = "Home";
-//   console.log(req.active);
-//   next();
-// });
-
 // app.engine("ejs", ejs.engine());
 app.use(expressLayouts);
 app.set("layout", "./layouts/defaultLayout");
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
+app.use("/js", express.static(path.join(__dirname, "static/js")));
 
 app.use("/", require(path.join(__dirname, "routes/scan.js")));
 
--- routes/scan.js
+++ routes/scan.js
@@ -1,14 +1,51 @@
 const express = require("express");
 const path = require("path");
+const QRCode = require("qrcode");
 
 const router = express.Router();
 
+// const generateQR = async (text) => {
+//   try {
+//     const val = await QRCode.toDataURL(text);
+//     return val;
+//   } catch (err) {
+//     return err;
+//   }
+// };
+
 router.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    done: false,
+    active: "home",
+  });
 });
 
 router.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    active: "about",
+  });
+});
+
+router.post("/qr-details", (req, res) => {
+  // let qr = generateQR(req.body.email);
+  // const qr = (async () => {
+  //   const val = await QRCode.toDataURL(req.body.email);
+  //   console.log(val);
+  // })().catch((err) => console.log(err));
+
+  const qr = QRCode.toDataURL("req.body.email")
+    .then((url) => {
+      url;
+    })
+    .catch((err) => {
+      url;
+    });
+  console.log(qr);
+  res.render("home", {
+    done: true,
+    active: "home",
+    qr: qr,
+  });
 });
 
 module.exports = router;
